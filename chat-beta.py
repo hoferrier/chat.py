f599f8245de0295527bf5ecf709037f9c23c5c87
Henry Ferrier <hofer1@student.monash.edu>
2016-10-04 02:50:57 +1100

    stability improvements. it connects most of the time now.

--- chat-beta.py
+++ chat-beta.py
@@ -51,25 +51,6 @@ def get_self_name():
         return input('screen name: '.rjust(round(term.width / 2)))
 
 
-def startup():
-    print(term.clear)
-    with term.location(0, int(term.height/2)):
-        print(('connecting to ' + partner_ip + '...').center(term.width))
-
-
-def main():
-    startup()
-
-    threading.Thread(target=sender).start()
-    # threading.Thread(target=logger).start()
-
-    log.append((datetime.now().strftime('%H:%M:%S'), 'System', 'connected to ' + partner_ip))
-
-    writer()
-
-    print(term.exit_fullscreen)
-
-
 def local_sender(term, var):
     while True:
         with term.location(0, term.height-1):
@@ -107,6 +88,7 @@ def logger():
 
 def receiver():
     global partner_name
+    global rcv_status
     last_error = None
     connection = None
     sock_rcv.bind(self_addr)
@@ -114,10 +96,11 @@ def receiver():
         try:
             sock_rcv.listen()
             connection, client_address = sock_rcv.accept()
+            rcv_status = True
         except OSError:
             if last_error is not OSError:
                 last_error = OSError
-                log.append((datetime.now().strftime('%H:%M:%S'), 'System', 'Failed to receive connection.'))
+                log.append((datetime.now().strftime('%H:%M:%S'), ERR_NAME, 'Failed to receive connection.'))
 
     while True:
         data = connection.recv(4096)
@@ -127,50 +110,51 @@ def receiver():
                 text = text[1:].split(' ')
                 if text[0] == 'setname':
                     partner_name = text[1]
-                    log.append((datetime.now().strftime('%H:%M:%S'), 'System', 'Connected to {}.'.format(partner_name)))
+                    log.append((datetime.now().strftime('%H:%M:%S'), SYS_NAME, 'Connected to {}.'.format(partner_name)))
                 if text[0] == 'q':
-                    log.append((datetime.now().strftime('%H:%M:%S'), 'System', '{} has closed the connection.'.format(partner_name)))
+                    log.append((datetime.now().strftime('%H:%M:%S'), SYS_NAME, '{} has closed the connection. Press any key to exit.'.format(partner_name)))
             else:
                 log.append((datetime.now().strftime('%H:%M:%S'), partner_name, text))
 
 
 def sender():
+    global snd_status
     last_error = None
 
-    connected = False
-    while not connected:
+    while not snd_status:
         try:
             sock_snd.connect(partner_addr)
             sock_snd.sendall(bytearray('-setname ' + self_name, 'utf-8'))
-            connected = True
+            snd_status = True
         except ConnectionRefusedError as e:
             if last_error is not ConnectionRefusedError:
                 last_error = e
-                log.append((datetime.now().strftime('%H:%M:%S'), 'System', 'Failed to send connection.'))
+                log.append((datetime.now().strftime('%H:%M:%S'), ERR_NAME, 'Failed to send connection.'))
             time.sleep(1)
         except BrokenPipeError as e:
             if last_error is not BrokenPipeError:
                 last_error = e
-                log.append((datetime.now().strftime('%H:%M:%S'), 'System', 'Failed to send data.'))
+                log.append((datetime.now().strftime('%H:%M:%S'), ERR_NAME, 'Failed to send data.'))
             time.sleep(1)
         except OSError:
             if last_error is not OSError:
                 last_error = OSError
-                log.append((datetime.now().strftime('%H:%M:%S'), 'System', 'Problem with partner\'s IP address.'))
+                log.append((datetime.now().strftime('%H:%M:%S'), ERR_NAME, 'Problem with partner\'s IP address.'))
             time.sleep(1)
 
     exit = False
     while not exit:
         with term.location(0, term.height-1):
             message = input()
-        if message.startswith('-'):
-            if message.strip('-') == 'q':
-                exit = True
-        try:
-            sock_snd.sendall(bytearray(message, 'utf-8'))
-            log.append((datetime.now().strftime('%H:%M:%S'), self_name, message))
-        except BrokenPipeError:
-            log.append((datetime.now().strftime('%H:%M:%S'), 'System', 'Message "{0}" not sent.'.format(message)))
+        if message:
+            if message.startswith('-'):
+                if message.strip('-') == 'q':
+                    exit = True
+            try:
+                sock_snd.sendall(bytearray(message, 'utf-8'))
+                log.append((datetime.now().strftime('%H:%M:%S'), self_name, message))
+            except BrokenPipeError:
+                log.append((datetime.now().strftime('%H:%M:%S'), SYS_NAME, 'Message "{}" not sent.'.format(message)))
 
 
 def writer():
@@ -195,12 +179,14 @@ def writer():
 
 
 def colorise(item, max_char):
-    if item[1] == 'System':
-        out_string = term.red + '[' + item[0] + '] ' + item[1].rjust(max_char) + ': ' + item[2] + term.normal
+    if item[1] == SYS_NAME:
+        out_string = term.bright_black('[{}] {}: {}'.format(item[0], item[1].rjust(max_char), item[2]))
+    elif item[1] == ERR_NAME:
+        out_string = term.red('[{}] {}: {}'.format(item[0], item[1].rjust(max_char), item[2]))
     elif item[2][0] == '>':
-        out_string = '[' + item[0] + '] ' + item[1].rjust(max_char) + ': ' + term.green + item[2] + term.normal
+        out_string = '[{}] {}: {}'.format(item[0], item[1].rjust(max_char), term.green(item[2]))
     else:
-        out_string = '[' + item[0] + '] ' + item[1].rjust(max_char) + ': ' + str(item[2])
+        out_string = '[{}] {}: {}'.format(item[0], item[1].rjust(max_char), item[2])
 
     if len(out_string) > term.width:
         for i in range(term.width, len(out_string), term.width):
@@ -214,19 +200,49 @@ if __name__ == '__main__':
                   'S Y N E R G Y',
                   'no girls allowed',
                   'make local networking great again']
-    partner_name = 'Anon'
-    sock_rcv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock_snd = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    rcv_msgs = {False: '⟳ connecting downstream...', True: '✓ connected downstream'}
+    snd_msgs = {False: '⟳ connecting upstream...', True: '✓ connected upstream'}
+    SYS_NAME = 'chat.py'
+    ERR_NAME = 'error'
     log = []
     term = Terminal()
+
+    sock_rcv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    rcv_status = False
+
+    sock_snd = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    snd_status = False
+
+    partner_name = ''
+    partner_ip = ''
+    partner_port = 10008
+
+    self_name = ''
+    self_ip = ''
+    self_port = 10007
+
     print(term.enter_fullscreen)
 
     self_ip = get_self_ip()
-    self_addr = (self_ip, 10007)
+    self_addr = (self_ip, self_port)
     threading.Thread(target=receiver).start()
 
     partner_ip = get_partner_ip()
-    partner_addr = (partner_ip, 10008)
+    partner_addr = (partner_ip, partner_port)
     self_name = get_self_name()
 
-    main()
+    threading.Thread(target=sender).start()
+
+    while not rcv_status or not snd_status:
+        print(term.clear)
+        with term.location(0, int((term.height / 2) - 1)):
+            print(rcv_msgs[rcv_status].center(term.width))
+            print(snd_msgs[snd_status].center(term.width))
+        time.sleep(1)
+
+    log.append((datetime.now().strftime('%H:%M:%S'), SYS_NAME, 'connected to ' + partner_name))
+
+    writer()
+
+    print(term.exit_fullscreen)
+
